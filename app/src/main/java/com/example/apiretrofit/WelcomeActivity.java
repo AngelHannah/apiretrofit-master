4e07d1c90a31976fb446e057500d43d9bd50456a
Hannah <87335534+AngelHannah@users.noreply.github.com>
2024-06-18 23:37:19 -0700

    Update WelcomeActivity.java
    Added onclick listener for button to launch the main activity

--- app/src/main/java/com/example/apiretrofit/WelcomeActivity.java
+++ app/src/main/java/com/example/apiretrofit/WelcomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.apiretrofit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,18 @@ public class WelcomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Get the button
+        Button apiBtn = findViewById(R.id.button);
+
+        apiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create the intent and launch the main activity
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
